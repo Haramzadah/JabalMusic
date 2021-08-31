78d5b813ee4af6f7d920435a315ef41077413cfd
Kaum Halawa <88365831+Haramzadah@users.noreply.github.com>
2021-08-31 17:20:21 +0700

    config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ API_HASH = getenv("API_HASH")
 ARQ_API_KEY = getenv("ARQ_API_KEY", "QFOTZM-GSZUFY-CHGHRX-TDEHOZ-ARQ")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "GroupMusicRandom")
-OWNER = getenv("OWNER", "Tomi_sn")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "Update_Grouppp")
+OWNER = getenv("OWNER", "zaenmad")
 STICKER_ID = getenv(
     "STICKER_ID",
     "CAACAgUAAxkBAAFF-Bdg-i8JvMgppo9DCVkFV9pPVSprzgACdwIAAsgM0VdqqiQQ6Hdw7CAE",
 )
-BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/3cf32d3eae4c81dca0792.jpg")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/d3a5e5665caabdcf0317d.jpg")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
